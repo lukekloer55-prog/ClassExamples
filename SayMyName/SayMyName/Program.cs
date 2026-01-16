efd30f100667e7c510b717e36a207b44db273e5a
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-01-16 08:26:36 -0700

    Add files back

---
+++ SayMyName/SayMyName/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Luke Kloer
+Rcet 2265
+Semester 3
+RCET
+https://github.com/lukekloer55-prog/HelloWorld.git*/
+
+using System.Diagnostics;
+
+namespace SayMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name below.");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Hello {Name} it's nice to see you today!");
+        }
+    }
+}
