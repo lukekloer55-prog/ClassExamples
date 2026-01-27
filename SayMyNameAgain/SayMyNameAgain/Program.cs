766a7d50aa49f382f7f973b7ed19d6160c89f35e
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-01-27 08:31:16 -0700

    Finished SayMyNameAgain

---
+++ SayMyNameAgain/SayMyNameAgain/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Luke Kloer
+Rcet 2265
+Semester 3
+RCET
+https://github.com/lukekloer55-prog/HelloWorld.git*/
+
+namespace SayMyNameAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Please enter your name: ");
+                string userName = Console.ReadLine();
+                const string Emily = "Emily";
+                const String Joe = "Joe";
+                const String Luke = "Luke";
+                const String Tim = "Tim";
+                switch (userName)
+                {
+
+                    case Emily:
+                        Console.WriteLine("Hello, Emily how are you?");
+                        break;
+
+                    case Joe:
+                        Console.WriteLine("Hello, Joe how are you?");
+                        break;
+
+                    case Luke:
+                        Console.WriteLine("What is up gangsta!!");
+                        break;
+
+                    case Tim:
+                        Console.WriteLine("Hey Tim, my fellow vim yanker.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Your name is very generic sorry! ;(");
+                        break;
+                }
+            } while (Console.ReadKey().Key != ConsoleKey.Enter);
+
+        }
+    }
+}
