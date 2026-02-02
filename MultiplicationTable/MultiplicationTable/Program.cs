cad29461598702dbfb35ac1d1d77a35296764d16
Luke <Luke@DESKTOP-205ULS7>
2026-02-02 13:32:33 -0700

    Start to multiplication table

--- MultiplicationTable/MultiplicationTable/Program.cs
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MultiplicationTable
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace MultiplicationTable
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int rowLength = 12;
+            int colLength = 12;
+
+
+            for (int row =1; row <= rowLength; row++)
+            {
+                for (int col = 1; col <= colLength; col++)
+                {
+                    int product = row * col;
+                    Console.Write($"{product,4}");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }
