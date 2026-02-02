3632e1e781a43c78eec8dd7b31c8ffdc788f3033
Luke <Luke@DESKTOP-205ULS7>
2026-02-02 13:59:24 -0700

    Finished the fortune cookie, it needs a header

--- FortuneCookie/FortuneCookie/Program.cs
+++ FortuneCookie/FortuneCookie/Program.cs
@@ -1,13 +1,4 @@
-﻿/*Luke Kloer
-Rcet 2265
-Semester Spring 2026
-RCET 2265 - Programming in C#
-Fortune Cookie Program
-GitHub URL:
-https://github.com/lukekloer55-prog/ClassExamples/tree/simple/FortuneCookie */
-
-
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Security.Cryptography;
 
 namespace FortuneCookie
@@ -16,10 +7,7 @@ namespace FortuneCookie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to the fortune cookie simulator!!");
-            Console.WriteLine("Here is your fortune!!");
-            // Create a list of fortunes
-            List<string> fortunes = new List<string>();
+                List<string> fortunes = new List<string>();
             {
                 fortunes.Add("You will have a great day!");
                 fortunes.Add("Success is on the horizon.");
@@ -31,14 +19,10 @@ namespace FortuneCookie
                 fortunes.Add("A thrilling time is in your immediate future.");
                 fortunes.Add("Your creativity will lead you to success.");
             }
-
             Random rnd = new Random();
-            // Generate a random index to select a fortune
             int randomFortune = rnd.Next(0, 9);
-            // Display the selected fortune
+
             Console.WriteLine(fortunes[randomFortune]);
-            // Wait for user input before closing the console
-            Console.Read();
         }        
     }
 }
