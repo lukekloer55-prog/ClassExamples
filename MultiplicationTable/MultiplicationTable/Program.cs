fb35e35710e7730f9dfaeabe67e3c05c58ebdec4
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-02-03 10:02:44 -0700

    Adding user input to multiplication table

--- MultiplicationTable/MultiplicationTable/Program.cs
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization.Formatters;
+using System.Text.RegularExpressions;
 
 namespace MultiplicationTable
 {
@@ -6,8 +7,15 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Multiplication Table");
+            Console.WriteLine("Please choose the size of the table (e.g., 12x12 for a 12x12 table): ");
+            Console.WriteLine("Do this in the format, (value x value)");
+            string tableSize = Console.ReadLine();
+           
+
             int rowLength = 12;
-            int colLength = 12;
+            int colLength = 5;
 
 
             for (int row =1; row <= rowLength; row++)
