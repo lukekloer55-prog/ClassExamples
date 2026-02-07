f7de473f16675f9c54e9c64898124146e1b81dec
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-02-07 12:35:47 -0700

    Added the header to my fortune cookie simulator.

--- MultiplicationTable/MultiplicationTable/Program.cs
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -4,7 +4,7 @@ Semester Spring 2026
 RCET 2265 - Programming in C#
 Multiplication Table Program
 GitHub URL:
-https://github.com/lukekloer55-prog/ClassExamples/tree/main/BetterCalculator */
+https://github.com/lukekloer55-prog/ClassExamples/tree/simple/MultiplicationTable */
 
 
 using System.ComponentModel.Design;
@@ -18,11 +18,18 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            
+            // This program will create a multiplication table based on the size the user inputs. The user will input the size in the format of (value x value).
+            // The program will then split the input into two values and use those values to create a multiplication table.
+            // If the user inputs an incorrect format, the program will display an error message.
+
             Console.WriteLine("Multiplication Table");
             Console.WriteLine("Please choose the size of the table ");
             Console.WriteLine("Do this in the format, (value x value): ");
             string tableSize = Console.ReadLine();
+
+            // The program will check if the input contains the character "x" to ensure it is in the correct format.
+            // If it does, it will split the input into two values and attempt to parse them as integers. If the parsing is successful, it will create a multiplication table based on those values.
+            // If the parsing fails or if the input does not contain "x", it will display an error message.
 
             if (tableSize.Contains("x"))
             {
@@ -31,13 +38,15 @@ namespace MultiplicationTable
                 if (int.TryParse(substring[0], out int userRow) & (int.TryParse(substring[1], out int userCol)))
                 {
                     Console.WriteLine($"You have chosen a {userRow}x{userCol} multiplication table.");
-
                 }
+                
                 else
                 {
                     Console.WriteLine("The inputed value is in an incorrect format.");
-
                 }
+
+                // This is the parsing of the two values, if parsing is succesful those inputs will be used.
+                // If parsing fails, the program will display an error message.
 
                 for (int row = 1; row <= userRow; row++)
                 {
@@ -48,9 +57,11 @@ namespace MultiplicationTable
                     }
                     Console.WriteLine();
                 }
+                // This is the creation of the multiplication table,
+                // it uses nested for loops to iterate through the rows and columns and calculate the product of the current row and column.
+                // The product is then printed with padding for alignment.
 
                 Console.ReadLine();
-
             }
 
             else 
@@ -60,7 +71,7 @@ namespace MultiplicationTable
                 Console.WriteLine("The inputed value is in an incorrect format.");
 
                 }
-
+            // This is the error message that will be displayed if the input does not contain "x".
         }
     }
 }
