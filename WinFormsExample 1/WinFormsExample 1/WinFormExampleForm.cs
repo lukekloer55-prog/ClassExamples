48314722cb74021d6c5f38a926d3269824379765
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-02-25 08:50:34 -0700

    Added text box color change so that when text is not added the box is yellow but once text is changed the box goes to white.

--- WinFormsExample 1/WinFormsExample 1/WinFormExampleForm.cs
+++ WinFormsExample 1/WinFormsExample 1/WinFormExampleForm.cs
@@ -33,52 +33,56 @@ namespace WinFormsExample_1
         {
 
             NameTextBox.Text = "";
+            NameTextBox.BackColor = Color.LightYellow;
             AgeTextBox.Text = "";
             CityTextBox.Text = "";
             PhoneTextBox.Text = "";
-
             UpperCaseRadio.Checked = true;
-        }
-
-        private void radioButton6_CheckedChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void ValidateFields ()
         {
-
             string message = "";
-
             if (CityTextBox.Text == "")
             {
                 message = "City is required\n" + message;
                 CityTextBox.Focus();
             }
-
             if (PhoneTextBox.Text == "")
             {
                 message = "Phone is required\n" + message;
                 PhoneTextBox.Focus();
             }
-
             if (AgeTextBox.Text == "")
             {
                 message = "Age is required\n" + message;
                 AgeTextBox.Focus();
             }
-
             if (NameTextBox.Text == "")
             {
                message = "Name is required\n" + message;
                NameTextBox.Focus();
             }
-
             if (message != "")
             {
                 MessageBox.Show(message);
             }
+        }
+
+        private void NameTextBox_TextChanged(Object sender, EventArgs e)
+        {
+            if (NameTextBox.Text != "")
+            {
+                NameTextBox.BackColor = Color.White;
+            }
+
+            else
+            { 
+                NameTextBox.BackColor = Color.LightYellow; 
+            }
 
         }
+
+
     }
 }
