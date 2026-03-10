1e99ed5e4d0fb406cf1b672c305cc174db7936dd
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-03-10 08:55:02 -0600

    Built a list method

--- ListExamples/ListExamples/ListExamplesForm.cs
+++ ListExamples/ListExamples/ListExamplesForm.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace ListExamples
 {
     public partial class ListExamplesForm : Form
@@ -6,7 +8,16 @@ namespace ListExamples
         {
             InitializeComponent();
         }
+        //Custom Methods below here
 
+        void ListExamplesForm_Load(object sender, EventArgs e)
+        {
+            List<string> names = new List<string>();
+            names.Add("John");
+            names.Add("Jane");
+            names.Add("Jack");
+            this.Text = names.Count.ToString();
+        }
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -14,7 +25,7 @@ namespace ListExamples
 
         private void SubmitButton_Click(object sender, EventArgs e)
         {
-
+            ListExamplesForm_Load(sender, e);
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
