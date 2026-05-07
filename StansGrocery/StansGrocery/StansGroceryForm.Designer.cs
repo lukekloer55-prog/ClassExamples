5173001787453766e7f128023e88ed52defed44f
Luke <Luke@DESKTOP-205ULS7>
2026-05-07 10:39:46 -0600

    Entered comments and added my about form. Had to fix the default state start up with the show all button. As at startup it would show a 0 in the combo box signaling that the aisle filter was activated. This was done by making sure show all was default, when both the radio buttons were not turned on. Also added the about form but this was a simple copy and paste from the last assignment. I could not figure out how to do the splash form and everytime i tried it either didn't display or crashed. Also added the top menu items, and added comments. The formatting for the controls went away and I had to reassign the "click" controls back to all their respected buttons, but hopefully that carries on without error.

--- StansGrocery/StansGrocery/StansGroceryForm.Designer.cs
+++ StansGrocery/StansGrocery/StansGroceryForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace StansGrocery
+﻿using System.Windows.Forms;
+
+namespace StansGrocery
 {
     partial class StansGroceryForm
     {
@@ -32,32 +34,41 @@
             this.DisplayListBox = new System.Windows.Forms.ListBox();
             this.FilterByAisleRadioButton = new System.Windows.Forms.RadioButton();
             this.FilterByCategoryRadioButton = new System.Windows.Forms.RadioButton();
-            this.ChoiceListBox = new System.Windows.Forms.ListBox();
             this.SearchTextBox = new System.Windows.Forms.TextBox();
             this.SearchButton = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
+            this.SearchLabel = new System.Windows.Forms.Label();
+            this.FilterLabel = new System.Windows.Forms.Label();
+            this.TopMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.MenuTopMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SearchTopMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitTopMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpTopMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutTopStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TopMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // ChoiceComboBox
             // 
             this.ChoiceComboBox.FormattingEnabled = true;
-            this.ChoiceComboBox.Location = new System.Drawing.Point(157, 379);
+            this.ChoiceComboBox.Location = new System.Drawing.Point(82, 418);
             this.ChoiceComboBox.Name = "ChoiceComboBox";
             this.ChoiceComboBox.Size = new System.Drawing.Size(121, 21);
             this.ChoiceComboBox.TabIndex = 0;
+            this.ChoiceComboBox.SelectedIndexChanged += ChoiceComboBox_SelectedIndexChanged;
             // 
             // DisplayListBox
             // 
             this.DisplayListBox.FormattingEnabled = true;
-            this.DisplayListBox.Location = new System.Drawing.Point(82, 28);
+            this.DisplayListBox.Location = new System.Drawing.Point(18, 41);
             this.DisplayListBox.Name = "DisplayListBox";
-            this.DisplayListBox.Size = new System.Drawing.Size(617, 290);
+            this.DisplayListBox.Size = new System.Drawing.Size(770, 329);
             this.DisplayListBox.TabIndex = 1;
             // 
             // FilterByAisleRadioButton
             // 
             this.FilterByAisleRadioButton.AutoSize = true;
-            this.FilterByAisleRadioButton.Location = new System.Drawing.Point(27, 380);
+            this.FilterByAisleRadioButton.Location = new System.Drawing.Point(18, 386);
             this.FilterByAisleRadioButton.Name = "FilterByAisleRadioButton";
             this.FilterByAisleRadioButton.Size = new System.Drawing.Size(87, 17);
             this.FilterByAisleRadioButton.TabIndex = 2;
@@ -65,12 +76,11 @@
             this.FilterByAisleRadioButton.Text = "Filter By Aisle";
             this.FilterByAisleRadioButton.UseVisualStyleBackColor = true;
             this.FilterByAisleRadioButton.Click += FilterByAisleRadioButton_CheckedChanged;
-
             // 
             // FilterByCategoryRadioButton
             // 
             this.FilterByCategoryRadioButton.AutoSize = true;
-            this.FilterByCategoryRadioButton.Location = new System.Drawing.Point(27, 403);
+            this.FilterByCategoryRadioButton.Location = new System.Drawing.Point(124, 386);
             this.FilterByCategoryRadioButton.Name = "FilterByCategoryRadioButton";
             this.FilterByCategoryRadioButton.Size = new System.Drawing.Size(107, 17);
             this.FilterByCategoryRadioButton.TabIndex = 3;
@@ -79,24 +89,16 @@
             this.FilterByCategoryRadioButton.UseVisualStyleBackColor = true;
             this.FilterByCategoryRadioButton.Click += FilterByCategoryRadioButton_CheckedChanged;
             // 
-            // ChoiceListBox
-            // 
-            this.ChoiceListBox.FormattingEnabled = true;
-            this.ChoiceListBox.Location = new System.Drawing.Point(320, 383);
-            this.ChoiceListBox.Name = "ChoiceListBox";
-            this.ChoiceListBox.Size = new System.Drawing.Size(186, 17);
-            this.ChoiceListBox.TabIndex = 4;
-            // 
             // SearchTextBox
             // 
-            this.SearchTextBox.Location = new System.Drawing.Point(538, 380);
+            this.SearchTextBox.Location = new System.Drawing.Point(337, 414);
             this.SearchTextBox.Name = "SearchTextBox";
             this.SearchTextBox.Size = new System.Drawing.Size(136, 20);
             this.SearchTextBox.TabIndex = 5;
             // 
             // SearchButton
             // 
-            this.SearchButton.Location = new System.Drawing.Point(389, 335);
+            this.SearchButton.Location = new System.Drawing.Point(495, 408);
             this.SearchButton.Name = "SearchButton";
             this.SearchButton.Size = new System.Drawing.Size(145, 30);
             this.SearchButton.TabIndex = 6;
@@ -106,7 +108,7 @@
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(540, 335);
+            this.ExitButton.Location = new System.Drawing.Point(646, 408);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(145, 30);
             this.ExitButton.TabIndex = 7;
@@ -114,21 +116,94 @@
             this.ExitButton.UseVisualStyleBackColor = true;
             this.ExitButton.Click += ExitButton_Click;
             // 
+            // SearchLabel
+            // 
+            this.SearchLabel.AutoSize = true;
+            this.SearchLabel.Location = new System.Drawing.Point(290, 417);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(41, 13);
+            this.SearchLabel.TabIndex = 8;
+            this.SearchLabel.Text = "Search";
+            // 
+            // FilterLabel
+            // 
+            this.FilterLabel.AutoSize = true;
+            this.FilterLabel.Location = new System.Drawing.Point(47, 421);
+            this.FilterLabel.Name = "FilterLabel";
+            this.FilterLabel.Size = new System.Drawing.Size(29, 13);
+            this.FilterLabel.TabIndex = 9;
+            this.FilterLabel.Text = "Filter";
+            // 
+            // TopMenuStrip
+            // 
+            this.TopMenuStrip.Font = new System.Drawing.Font("Source Code Pro", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TopMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.TopMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuTopMenuItem,
+            this.HelpTopMenuItem});
+            this.TopMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.TopMenuStrip.Name = "TopMenuStrip";
+            this.TopMenuStrip.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.TopMenuStrip.Size = new System.Drawing.Size(800, 29);
+            this.TopMenuStrip.TabIndex = 10;
+            // 
+            // MenuTopMenuItem
+            // 
+            this.MenuTopMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SearchTopMenuItem,
+            this.ExitTopMenuItem});
+            this.MenuTopMenuItem.Name = "MenuTopMenuItem";
+            this.MenuTopMenuItem.Size = new System.Drawing.Size(54, 25);
+            this.MenuTopMenuItem.Text = "Menu";
+            // 
+            // SearchTopMenuItem
+            // 
+            this.SearchTopMenuItem.Name = "SearchTopMenuItem";
+            this.SearchTopMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.SearchTopMenuItem.Text = "&Search";
+            this.SearchTopMenuItem.Click += SearchButton_Click;
+            // 
+            // ExitTopMenuItem
+            // 
+            this.ExitTopMenuItem.Name = "ExitTopMenuItem";
+            this.ExitTopMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.ExitTopMenuItem.Text = "E&xit";
+            this.ExitTopMenuItem.Click += ExitButton_Click;
+            // 
+            // HelpTopMenuItem
+            // 
+            this.HelpTopMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutTopStripMenuItem});
+            this.HelpTopMenuItem.Name = "HelpTopMenuItem";
+            this.HelpTopMenuItem.Size = new System.Drawing.Size(54, 25);
+            this.HelpTopMenuItem.Text = "&Help";
+            // 
+            // AboutTopStripMenuItem
+            // 
+            this.AboutTopStripMenuItem.Name = "AboutTopStripMenuItem";
+            this.AboutTopStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.AboutTopStripMenuItem.Text = "&About";
+            this.AboutTopStripMenuItem.Click += AboutTopStripMenuItem_Click;
+            // 
             // StansGroceryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TopMenuStrip);
+            this.Controls.Add(this.FilterLabel);
+            this.Controls.Add(this.SearchLabel);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.SearchButton);
             this.Controls.Add(this.SearchTextBox);
-            this.Controls.Add(this.ChoiceListBox);
             this.Controls.Add(this.FilterByCategoryRadioButton);
             this.Controls.Add(this.FilterByAisleRadioButton);
             this.Controls.Add(this.DisplayListBox);
             this.Controls.Add(this.ChoiceComboBox);
             this.Name = "StansGroceryForm";
             this.Text = "Form1";
+            this.TopMenuStrip.ResumeLayout(false);
+            this.TopMenuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,10 +220,17 @@
         private System.Windows.Forms.ListBox DisplayListBox;
         private System.Windows.Forms.RadioButton FilterByAisleRadioButton;
         private System.Windows.Forms.RadioButton FilterByCategoryRadioButton;
-        private System.Windows.Forms.ListBox ChoiceListBox;
         private System.Windows.Forms.TextBox SearchTextBox;
         private System.Windows.Forms.Button SearchButton;
         private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.Label FilterLabel;
+        private System.Windows.Forms.MenuStrip TopMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem MenuTopMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SearchTopMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitTopMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpTopMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutTopStripMenuItem;
     }
 }
 
--- StansGrocery/StansGrocery/StansGroceryForm.cs
+++ StansGrocery/StansGrocery/StansGroceryForm.cs
@@ -6,6 +6,7 @@ Stans Grocery Program
 GitHub URL:
 https://github.com/lukekloer55-prog/ClassExamples/tree/main/StansGrocery */
 
+using EtchASketch;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,11 +22,18 @@ namespace StansGrocery
 {
     public partial class StansGroceryForm : Form
     {
+        // This is a two-dimensional array that will hold the customer data loaded from the file.
         string[,] customerData = new string[0, 0];
         string filePath = "..\\..\\..\\Grocery.txt";
 
         public StansGroceryForm()
         {
+            // When the form is initialized, we want to set up the default states of the controls,
+            // load the data from the file into the customerData array,
+            // populate the filter combo box with the appropriate values,
+            // and display the initial data in the DisplayListBox.
+            // This will default to showing all items since no filter is selected at this point.
+            // Gives the user an idea of all the avaliable items in the grocery store before they apply any filters or search terms.
             InitializeComponent();
             SetDefaults();
             FileToArray(filePath);
@@ -34,53 +42,19 @@ namespace StansGrocery
             DisplayData();
         }
 
-        private void SetDefaults()
-        {
-           
-            FilterByAisleRadioButton.Checked = false; // IMPORTANT FIX
-            FilterByCategoryRadioButton.Checked = false;
-        }
-
-        
-        int CountOfLinesIn(string filePath)
-        {
-            int count = 0;
-
-            using (StreamReader sr = new StreamReader(filePath))
-            {
-                while (sr.ReadLine() != null)
-                    count++;
-            }
-
-            return count;
-        }
-
-        
-        string CleanField(string input)
-        {
-            return input
-                .Replace("\"", "")
-                .Replace("$", "")
-                .Replace("#", "")
-                .Replace("%", "")
-                .Replace("ITM", "")
-                .Replace("LOC", "")
-                .Replace("CAT", "")
-                .Trim();
-        }
-
-      
         void FileToArray(string filePath)
         {
+            // This method reads data from a specified file and stores it in a two-dimensional array called customerData.
             int rows = CountOfLinesIn(filePath);
-
+            // The number of columns is set to 4, which corresponds to the expected fields in the data (e.g., item name, aisle, category, etc.).
             string[,] _customerData = new string[4, rows];
-
+            // A StreamReader is used to read the file line by line.
+            // For each line, the data is split by commas and stored in the corresponding position in the _customerData array after being cleaned using the CleanField method.
             using (StreamReader sr = new StreamReader(filePath))
             {
                 string line;
                 int counter = 0;
-
+                // Read through the file line by line until the end is reached, splitting each line by commas and storing the cleaned data in the _customerData array.
                 while ((line = sr.ReadLine()) != null)
                 {
                     string[] temp = line.Split(',');
@@ -93,31 +67,76 @@ namespace StansGrocery
                     counter++;
                 }
             }
-
+            
             customerData = _customerData;
+        }
+
+
+        private void SetDefaults()
+        {
+            // This just sets the default states of the radio checkboxes to unchecked when the form loads, so that no filter is applied until the user selects one.
+            FilterByAisleRadioButton.Checked = false; 
+            FilterByCategoryRadioButton.Checked = false;
+        }
+
+        
+        int CountOfLinesIn(string filePath)
+        {
+            int count = 0;
+            // This method counts the number of lines in a specified file.
+            // It initializes a counter variable to zero and uses a StreamReader to read through the file line by line.
+            // For each line that is read, the counter is incremented. Once the end of the file is reached, the total count of lines is returned.
+            // This is later used to determine the number of rows needed for the customerData array when loading the data from the file.
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                // Read through the file line by line until the end is reached, incrementing the count for each line.
+                while (sr.ReadLine() != null)
+                    count++;
+            }
+
+            return count;
+        }
+
+        
+        string CleanField(string input)
+        {
+            // This method takes an input string and removes specific unwanted characters and substrings, such as quotes, dollar signs, hash symbols, percentage signs, and certain prefixes like "ITM", "LOC", and "CAT".
+            // It also trims any leading or trailing whitespace from the resulting string. The cleaned string is then returned.
+            return input
+                .Replace("\"", "")
+                .Replace("$", "")
+                .Replace("#", "")
+                .Replace("%", "")
+                .Replace("ITM", "")
+                .Replace("LOC", "")
+                .Replace("CAT", "")
+                .Trim();
         }
 
        
         void DisplayData()
         {
+            // Clear the DisplayListBox to prepare for showing the filtered data.
             DisplayListBox.Items.Clear();
-
-            if (customerData.GetLength(1) == 0 || ChoiceComboBox.SelectedItem == null)
-                return;
-
+            // Declare a variable selectedFilter to store the currently selected filter value from the ChoiceComboBox.
+            // This will be used to determine which items to display based on the selected filter.
             string selectedFilter = ChoiceComboBox.SelectedItem.ToString();
+            // Declare a boolean variable showAll to determine whether to show all items or filter based on the selected filter.
             bool showAll = ChoiceComboBox.SelectedIndex == 0;
-
+            // Declare a variable search to store the search term entered in the SearchTextBox. 
             string search = SearchTextBox.Text ?? "";
 
+            // Loop through each row of the customerData array to check for matches with the selected filter and search term.
             for (int row = 0; row < customerData.GetLength(1); row++)
             {
+                // Check if the first column of the current row is null. If it is, skip to the next iteration of the loop.
                 if (customerData[0, row] == null)
+                    // If the item name is null, we skip this row since it doesn't contain valid data.
+                    // Continue is used to skip the rest of the loop body and move on to the next iteration, which allows us to avoid processing rows that don't have valid item names.
                     continue;
 
-               
                 string item;
-
+                // Determine how to format the item string based on which filter is selected.
                 if (FilterByAisleRadioButton.Checked)
                 {
                     item = $"{customerData[0, row]} - {customerData[1, row]}";
@@ -130,17 +149,20 @@ namespace StansGrocery
                 {
                     item = $"{customerData[0, row]} - {customerData[1, row]} - {customerData[2, row]}";
                 }
-
-              
+                // Determine which column to use for filtering based on the selected filter radio button.
+                // The question mark operator is used to check if the FilterByCategoryRadioButton is checked.
+                // If it is, filterColumn is set to 2 (the category column); otherwise, it is set to 1 (the aisle column).
+                // Acts as a if statement in one line, where if the condition before the question mark is true, the value after the question mark is assigned to filterColumn, and if it is false, the value after the colon is assigned to filterColumn.
+                // Found this solution on somones blog.
                 int filterColumn = FilterByCategoryRadioButton.Checked ? 2 : 1;
-
+                // Check if the current row matches the selected filter and search term.
                 bool matchesFilter =
                     showAll || customerData[filterColumn, row] == selectedFilter;
-
-             
+                // Check if the item string contains the search term, ignoring case.
+                // This ignore case is different than the class examples as this version is older and doesn't have the string.Contains method.
                 bool matchesSearch =
                     item.IndexOf(search, StringComparison.InvariantCultureIgnoreCase) >= 0;
-
+                // If the current row matches both the selected filter and the search term, add it to the DisplayListBox.
                 if (matchesFilter && matchesSearch)
                 {
                     DisplayListBox.Items.Add(item);
@@ -151,29 +173,58 @@ namespace StansGrocery
        
         void LoadFilterComboBox()
         {
+            // Clear existing items
             ChoiceComboBox.Items.Clear();
-            ChoiceComboBox.Items.Add("Show All");
-
+            // If no filter is selected, just show "Show All"
+            // Fixed the problem with aisles being displayed when nothing was selected.
+            if (!FilterByAisleRadioButton.Checked && !FilterByCategoryRadioButton.Checked)
+            {
+                ChoiceComboBox.Items.Add("Show All");
+                ChoiceComboBox.SelectedIndex = 0;
+                return;
+            }
+            // Determine which column to extract values from based on the selected filter
             int column = FilterByCategoryRadioButton.Checked ? 2 : 1;
-
+            //
+            List<string> values = new List<string>();
+            
             for (int row = 0; row < customerData.GetLength(1); row++)
             {
+                // Skip null or empty values
                 string value = customerData[column, row];
-
-                if (!string.IsNullOrEmpty(value) &&
-                    !ChoiceComboBox.Items.Contains(value))
+                // Add unique values to the list
+                if (!string.IsNullOrEmpty(value) && !values.Contains(value))
                 {
-                    ChoiceComboBox.Items.Add(value);
+                    values.Add(value);
                 }
             }
-
-            ChoiceComboBox.Sorted = true;
-            ChoiceComboBox.SelectedIndex = 0;
+            // Sort numerically if possible, otherwise alphabetically
+            // Had to use online repositories to find a way to sort the values in the combo box numerically if they were numbers, and alphabetically if they were not.
+            // I found a solution that uses a custom comparison function in the Sort method of the List<string>.
+            values.Sort((a, b) =>
+            {
+                // Try to parse both values as integers
+                bool aIsNum = int.TryParse(a, out int aNum);
+                bool bIsNum = int.TryParse(b, out int bNum);
+                // If both are numbers, compare them as integers
+                if (aIsNum && bIsNum) return aNum.CompareTo(bNum);
+                // Otherwise, compare them as strings
+                return string.Compare(a, b, StringComparison.InvariantCultureIgnoreCase);
+            });
+            // Add "Show All" option at the top of the sorted list
+            ChoiceComboBox.Sorted = false;
+            // Set Sorted to false to maintain the order of items as we add them, since we are adding "Show All" at the top.
+            ChoiceComboBox.Items.Add("Show All");
+            // Add the sorted values to the combo box
+            foreach (string value in values) 
+                ChoiceComboBox.Items.Add(value);
+                ChoiceComboBox.SelectedIndex = 0;
         }
 
        
         private void ChoiceComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // When the selected index changes, we want to update the displayed data based on the new filter selection.
             DisplayData();
         }
 
@@ -184,6 +235,7 @@ namespace StansGrocery
 
         private void FilterByAisleRadioButton_CheckedChanged(object sender, EventArgs e)
         {
+            // When the aisle filter is selected, we want to load the combo box with the unique aisle values and update the displayed data accordingly.
             LoadFilterComboBox();
             ChoiceComboBox.SelectedIndex = 0;
             DisplayData();
@@ -191,6 +243,7 @@ namespace StansGrocery
 
         private void FilterByCategoryRadioButton_CheckedChanged(object sender, EventArgs e)
         {
+            // When the category filter is selected, we want to load the combo box with the unique category values and update the displayed data accordingly.
             LoadFilterComboBox();
             ChoiceComboBox.SelectedIndex = 0;
             DisplayData();
@@ -198,41 +251,56 @@ namespace StansGrocery
 
         private void SearchButton_Click(object sender, EventArgs e)
         {
+            // Declare the search variable and delete any leading or trailing whitespace from the search term entered in the SearchTextBox.
             string search = SearchTextBox.Text.Trim();
-
+            // Clear the DisplayListBox to prepare for showing the search results.
             DisplayListBox.Items.Clear();
-
+            // Check if the search term is empty or consists only of whitespace.
+            // If it is, show a message box prompting the user to enter a search term and return from the method to prevent further execution.
             if (string.IsNullOrWhiteSpace(search))
             {
                 MessageBox.Show ("Please enter a search term.");
                 return;
             }
-
+            // Initialize a boolean variable foundAny to keep track of whether any matches were found during the search.
             bool foundAny = false;
-
+            // Loop through each row of the customerData array to check for matches with the search term.
             for (int row = 0; row < customerData.GetLength(1); row++)
             {
-                 
                     string name = customerData[0, row];
                     string aisle = customerData[1, row];
                     string category = customerData[2, row];
-
-                    if ((name != null && name.IndexOf(search, StringComparison.InvariantCultureIgnoreCase) >= 0) ||
+                // Check if any of the fields (name, aisle, category) contain the search term, ignoring case.
+                // If a match is found, add the item to the DisplayListBox and set foundAny to true.
+                if ((name != null && name.IndexOf(search, StringComparison.InvariantCultureIgnoreCase) >= 0) ||
                         (aisle != null && aisle.IndexOf(search, StringComparison.InvariantCultureIgnoreCase) >= 0) ||
                         (category != null && category.IndexOf(search, StringComparison.InvariantCultureIgnoreCase) >= 0))
                     {
                         DisplayListBox.Items.Add($"{name} - {aisle} - {category}");
                         foundAny = true;
                     }
-                
             }
-
+            // After the loop, check if foundAny is still false, which means no matches were found.
+            // If so, show a message box informing the user that there are no matches for the search term.
             if (!foundAny)
             {
                 MessageBox.Show($"Sorry no matches for {search}");
                 
             }
            
+        }
+
+        private void AboutTopStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutForm = new AboutForm(); //instantitate the about form
+            aboutForm.FormClosed += AboutForm_FormClosed; //map the FormClosed event to a handler
+            aboutForm.Show(); // show the about form
+            this.Hide();// hide the main form while the about form is open
+        }
+
+        private void AboutForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Show(); // show the main form again when the about form is closed
         }
     }
 }
